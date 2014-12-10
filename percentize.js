8c745e7b64500bf37e2313a500337b572947cad9
Lance Vo <lvo@vinavo.us>
2014-12-10 10:06:10 -0600

    Update percentize.js

--- percentize.js
+++ percentize.js
@@ -1,4 +1,4 @@
-angular.module('percentize',[]).filter('percentize', function () {
+angular.module('ng-percentize',[]).filter('percentize', function () {
   return function (input, decimal) {
     if (typeof decimal === 'undefined') {
       decimal = 2;
