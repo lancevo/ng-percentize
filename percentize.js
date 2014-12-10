2d344da1298d95c52b3854b809c9c91b791a962b
Lance Vo <lvo@vinavo.us>
2014-12-10 09:53:59 -0600

    Create percentize.js

---
+++ percentize.js
@@ -0,0 +1,8 @@
+angular.module('percentize',[]).filter('percentize', function () {
+  return function (input, decimal) {
+    if (typeof decimal === 'undefined') {
+      decimal = 2;
+    }
+    return isNaN(input) ? '' : (input * 100).toFixed(decimal);
+  };
+});
