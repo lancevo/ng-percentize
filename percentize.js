e1e4662c26b648e6b72fd7f00882ae24bccf7743
Lance Vo <lvo@vinavo.us>
2015-02-25 13:01:49 -0600

    enhance result with toLocaleString
    ie: 123456.789  -> 123,456.78

--- percentize.js
+++ percentize.js
@@ -1,10 +1,14 @@
-// <https://github.com/lancevo/ng-percentize/>
-
 angular.module('ng-percentize',[]).filter('percentize', function () {
-  return function (input, decimal) {
-    if (typeof decimal === 'undefined') {
-      decimal = 2;
+  return function percentize(percent, digits) {
+    if (isNaN(percent)) {
+      return '';
     }
-    return isNaN(input) ? '' : (input * 100).toFixed(decimal);
+    if (typeof digits === 'undefined') {
+      digits = 2;
+    }
+    percent = percent * 100;
+    var integer = parseInt(percent, 10).toLocaleString(),
+        decimal = (percent % 1).toFixed(digits).replace('0','');
+    return integer + decimal;
   };
 });
